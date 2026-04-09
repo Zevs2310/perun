ddce9e5250cbc4c97cb04bfa7da83d143599fb36
Zevs2310 <31366421+Zevs2310@users.noreply.github.com>
2026-04-10 00:30:10 +0200

    Add device retrieval functions in device_service.py

--- backend/app/services/device_service.py
+++ backend/app/services/device_service.py
@@ -1,1 +1,10 @@
+from app.db.fake_db import devices
 
+def get_all_devices():
+    return devices
+
+def get_device_by_id(device_id: int):
+    for d in devices:
+        if d["id"] == device_id:
+            return d
+    return None
